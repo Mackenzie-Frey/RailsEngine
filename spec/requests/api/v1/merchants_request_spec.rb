cdb55b5d09ca313b343f1d1d199159890a82d10a
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-12 15:31:36 -0600

    Adds merchant request spec expectations.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 describe "Merchant API" do
   it "sends a list of merchants" do
     create_list(:merchant, 3)
+
     get '/api/v1/merchants.json'
 
     expect(response).to be_successful
 
-    merchants = response.body
+    merchants = JSON.parse(response.body)
 
-    expect(merchants.count).to eq(3)
-    expect(merchants.first).to have_key("id")
+    expect(merchants["data"].count).to eq(3)
+    expect(merchants["data"].first).to have_key("id")
+    expect(merchants["data"].first["type"]).to eq("merchant")
+    expect(merchants["data"].first["attributes"]["name"]).to eq(Merchant.first.name)
   end
 
   # find an id with the date in spec harness, and expect it to come out to as the spec harness expects
