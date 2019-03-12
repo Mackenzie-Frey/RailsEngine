2e5f364d374e46853790978c73b229ee43a454fb
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-11 23:25:33 -0600

    Adds expectation of JSON object being returned.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe "Merchant API" do
   it "sends a list of merchants" do
-    create_list(:merchants, 3)
+    create_list(:merchant, 3)
 
     get '/api/v1/merchants.json'
 
     expect(response).to be_successful
+
+    merchants = response.body["data"]
+
+    expect(merchants.count).to eq(3)
+    expect(merchants.first).to have_key("id")
   end
 
 end
