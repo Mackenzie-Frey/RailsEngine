fa3ca8be100162493e26935e6c8d0f26b5a95bcb
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-12 00:20:22 -0600

    Passes all model validation and relationship tests for transactions.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+
+  validates_presence_of :credit_card_number,
+                        :result,
+                        :created_at,
+                        :updated_at
 end
