736e77d8e36432b3dbae9166684f67a2d7cb4bdc
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-12 00:14:47 -0600

    Passes invoice validation and relationship tests.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,10 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+  has_many :transactions
+  has_many :invoice_items
+
+  validates_presence_of :status,
+                        :created_at,
+                        :updated_at
 end
