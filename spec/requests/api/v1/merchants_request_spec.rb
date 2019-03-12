4533fc0fb612f70e60c6c9c6175f4e1aab981ccf
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-12 15:35:42 -0600

    Adds merchant request test for show route.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -16,6 +16,21 @@ describe "Merchant API" do
     expect(merchants["data"].first["attributes"]["name"]).to eq(Merchant.first.name)
   end
 
+  it "sends a single merchant" do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants/1.json'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"].count).to eq(1)
+    expect(merchants["data"].first).to have_key("id")
+    expect(merchants["data"].first["type"]).to eq("merchant")
+    expect(merchants["data"].first["attributes"]["name"]).to eq(Merchant.first.name)
+  end
+
   # find an id with the date in spec harness, and expect it to come out to as the spec harness expects
   # when you use a finder, search for the date format from the spec harness
 
