a2d968ef53ab6af8e6fd865025ac5c004309ca03
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-12 15:31:08 -0600

    Adds merchant serializer.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: Merchant.all
-    #why is the below line done in the test and not the code?
-    JSON.parse(response.body)
+    render json: MerchantSerializer.new(Merchant.all)
   end
 end
