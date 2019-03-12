13f45eb6beb6937ac60531132a0ff536c6f0c477
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-12 14:00:45 -0600

    Changes data types to date time objects and integer.

--- db/migrate/20190312031155_create_transactions.rb
+++ db/migrate/20190312031155_create_transactions.rb
@@ -5,8 +5,6 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
       t.string :credit_card_number
       t.string :credit_card_expiration_date
       t.string :result
-      t.string :created_at
-      t.string :updated_at
 
       t.timestamps
     end
