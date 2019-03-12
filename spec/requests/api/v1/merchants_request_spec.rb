85ed1deb5ea225873c1ea4bd7f3af8136483a9be
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-12 14:01:33 -0600

    Adds comments for next steps.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe "Merchant API" do
   it "sends a list of merchants" do
     create_list(:merchant, 3)
-
     get '/api/v1/merchants.json'
 
     expect(response).to be_successful
@@ -14,4 +13,10 @@ describe "Merchant API" do
     expect(merchants.first).to have_key("id")
   end
 
+  # find an id with the date in spec harness, and expect it to come out to as the spec harness expects
+  # when you use a finder, search for the date format from the spec harness
+
+  # finders use strong params
+  # case insensitive searches: column type in postgres called ci text (instead of text)
+  # add the above to it.
 end
