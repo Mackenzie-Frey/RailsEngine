df52a2c946f5ad6248f608bb7ba5422ce5919122
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-13 14:18:50 -0600

    Adds most_expensive invoice method for the class. Utilizes successful transaction scope.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,6 @@
 class Invoice < ApplicationRecord
   enum status: ["shipped"]
-  
+
   belongs_to :customer
   belongs_to :merchant
   has_many :transactions
@@ -9,4 +9,14 @@ class Invoice < ApplicationRecord
   validates_presence_of :status,
                         :created_at,
                         :updated_at
+
+# needs testing
+  def self.most_expensive(limit = 5)
+    Invoice.select("invoices.*, sum(quantity*unit_price) AS revenue")
+    .joins(:invoice_items, :transactions)
+    .merge(Transaction.successful)
+    .group(:id)
+    .order("revenue DESC")
+    .limit(5)
+  end
 end
