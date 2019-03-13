7e94627b1e3fe9f0bfefc64cd233aa4984b06483
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-13 09:08:48 -0600

    Adds comments for query.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,5 +7,14 @@ class Merchant < ApplicationRecord
                         :updated_at
 
   def self.most_revenue(n)
+    # binding.pry
+    # joins(:invoices)
+    # .joins("transactions ON invoices.id=transactions.invoice_id")
+
+    # joins(invoice_items: :transactions)
+    # sum(:quantity * :unit_price AS :revenue)
+    # .where(result: "success")
+    # .order(revenue: :desc)
+    # .limit(n)
   end
 end
