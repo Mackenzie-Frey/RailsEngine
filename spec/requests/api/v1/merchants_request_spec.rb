8c4b0d5efd00fb3921f52a094697675add1cdd0b
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-12 18:13:33 -0600

    Adds test for top revenue merchants.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,8 +18,7 @@ describe "Merchant API" do
 
   it "sends a single merchant" do
     m1 = create(:merchant)
-    create(:merchant)
-    create(:merchant)
+    create_list(:merchant, 2)
 
     get "/api/v1/merchants/#{m1.id}.json"
 
@@ -29,5 +28,33 @@ describe "Merchant API" do
 
     expect(merchants["data"]["id"]).to eq(m1.id.to_s)
     expect(merchants["data"]["attributes"]["name"]).to eq(m1.name)
+  end
+
+  it 'returns the top x merchants ranked by total revenue' do
+    customer_1 = create(:customer)
+
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    merchant_3 = create(:merchant)
+
+    item_1 = create(:item, merchant: merchant_1)
+    item_2 = create(:item, merchant: merchant_2)
+    item_3 = create(:item, merchant: merchant_3)
+
+    invoice_1 = create(:invoice, customer: customer_1, merchant: merchant_1)
+    invoice_2 = create(:invoice, customer: customer_1, merchant: merchant_2)
+    invoice_3 = create(:invoice, customer: customer_1, merchant: merchant_3)
+
+    create(:invoice_item, item: item_1, invoice: invoice_1, quantity: 10, unit_price: 500)
+    create(:invoice_item, item: item_2, invoice: invoice_2, quantity: 20, unit_price: 500)
+    create(:invoice_item, item: item_3, invoice: invoice_3, quantity: 30, unit_price: 500)
+
+    create(:transaction, invoice: invoice_1)
+    create(:transaction, invoice: invoice_2)
+    create(:transaction, invoice: invoice_3, result: "failed")
+
+    get '/api/v1/merchants/most_revenue?quantity=2'
+
+    expect(result).to eq([merchant_2, merchant_1])
   end
 end
