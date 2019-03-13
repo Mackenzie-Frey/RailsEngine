cddb282807dff665b6835041a574c559608ad7e5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-13 09:10:05 -0600

    Adds show and private method.

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::Merchants::SearchController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.most_revenue(2))
   end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[merchant_params]))
+  end
+
+  private
+  def merchant_params
+    params.require(:merchant).permit(:id, :name, :created_at, :updated_at)
+  end
 end
