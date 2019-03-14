1ef28d406e0ab7971114557815461926d102ee14
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-14 10:28:54 -0600

    Adds revenue_by_date model test.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -39,13 +39,14 @@ RSpec.describe Merchant, type: :model do
       expect(Merchant.most_revenue(2)).to eq([merchant_2, merchant_1])
     end
   end
-  context 'Instance Methods' do
+  describe 'Instance Methods' do
     it ".revenue_by_date(date)" do
-      customer = create(:customer)
       merchant = create(:merchant)
-      create(:invoice, customer: customer, merchant: merchant, updated_at: "2012-03-25 09:54:09 UTC")
+      invoice = create(:invoice, merchant: merchant, updated_at: "2012-03-25 09:54:09 UTC")
+      create(:invoice_item, invoice: invoice, quantity: 5, unit_price: 5000)
+      create(:transaction, result: 'success', invoice: invoice)
 
-      expect(merchant.revenue_by_date("2012-03-25")).to eq(44444)
+      expect(merchant.revenue_by_date("2012-03-25").revenue).to eq(25000)
     end
   end
 end
