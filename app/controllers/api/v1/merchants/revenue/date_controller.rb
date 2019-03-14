09fab2a1d83f8e241dcaa1092c7e0b64943e118d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-14 10:28:07 -0600

    Adds controller and action for total revenue by date.

---
+++ app/controllers/api/v1/merchants/revenue/date_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Merchants::Revenue::DateController < ApplicationController
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    render json: MerchantRevenueByDateSerializer.new(@merchant.revenue_by_date(params[:date]))
+  end
+end
