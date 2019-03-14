0c7a5fbf4cf616fe2e1538adc45e39849cde327f
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-14 10:30:25 -0600

    Adds revenue by date instance method.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -16,6 +16,9 @@ class Merchant < ApplicationRecord
   end
 
   def revenue_by_date(date)
-    Merchant.first.invoices.select("sum(unit_price*quantity) AS revenue").joins(:invoice_items, :transactions).merge(Transaction.successful).where(updated_at: Date.parse(date).all_day)[0].revenue
+    invoices.select("sum(unit_price*quantity) AS revenue")
+    .joins(:invoice_items, :transactions)
+    .merge(Transaction.successful)
+    .where(updated_at: Date.parse(date).all_day)[0]
   end
 end
