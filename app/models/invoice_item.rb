c9970cf15e5e8878b25d9b2cfa7ab0e5bd9479ff
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-14 21:57:06 -0600

    Adds comments explaining current error message.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -10,4 +10,8 @@ class InvoiceItem < ApplicationRecord
   def self.revenue_by_date(date)
     InvoiceItem.joins(invoice: [:transactions]).select("sum(unit_price*quantity) AS total_revenue").merge(Transaction.successful).where(updated_at: Date.parse(date).all_day)
   end
+  # The InvoiceItem id is returning nil as the rspec error. And there is not a revenue method defined.
+  # When the revenue method is changed to total_revenue, in accordance with the alias and spec requirements,
+  # the alogrithim cannot divid nil by 100.
+
 end
