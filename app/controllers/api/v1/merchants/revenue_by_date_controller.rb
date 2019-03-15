eca2903f74fed15451fefe1fdeb4695f3ba9f6b1
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-14 21:49:56 -0600

    Adds index method for returns the total revenue for date x across all merchants

--- app/controllers/api/v1/merchants/revenue_by_date_controller.rb
+++ app/controllers/api/v1/merchants/revenue_by_date_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::Merchants::RevenueByDateController < ApplicationController
-
   def show
     merchant = Merchant.find(params[:merchant_id])
     render json: MerchantRevenueByDateSerializer.new(merchant.revenue_by_date(params[:date]))
+  end
+
+  def index
+    render json: MerchantRevenueByDateSerializer.new(InvoiceItem.revenue_by_date(params[:date]))
   end
 end
