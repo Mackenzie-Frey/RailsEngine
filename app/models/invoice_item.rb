9fbef965a2dae3f7ffcbb561a99d2078634b10b3
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-14 21:50:25 -0600

    Adds class method revenue_by_date(date). Not passing test.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -6,4 +6,8 @@ class InvoiceItem < ApplicationRecord
                         :unit_price,
                         :created_at,
                         :updated_at
+
+  def self.revenue_by_date(date)
+    InvoiceItem.joins(invoice: [:transactions]).select("sum(unit_price*quantity) AS total_revenue").merge(Transaction.successful).where(updated_at: Date.parse(date).all_day)
+  end
 end
