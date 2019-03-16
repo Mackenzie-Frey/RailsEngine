74d804bdcc0282baced3fcead68e9124a2814821
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-16 13:30:04 -0600

    Adds find_transactions method for relationship endpoint.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,10 @@ class Customer < ApplicationRecord
     def self.random
       all.shuffle.pop
     end
+
+    def self.find_transactions(customer_id)
+      select("transactions.*")
+      .joins(invoices: :transactions)
+      .where(id: customer_id)
+    end
 end
