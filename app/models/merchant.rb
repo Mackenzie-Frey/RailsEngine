6c7ba67d58292c5d81d5db42b12c61b687027bfc
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-16 10:06:38 -0600

    Adds favorite_merchant class method for endpoint /api/v1/merchants/:id/favorite_customer. Passes spec harness and request spec.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -24,8 +24,20 @@ class Merchant < ApplicationRecord
   def self.most_items(limit)
     joins(invoices: [:invoice_items, :transactions])
     .select("merchants.*, SUM(quantity) AS total_quantity")
-    .group(:id).merge(Transaction.successful)
+    .group(:id)
+    .merge(Transaction.successful)
     .order("total_quantity DESC")
     .limit(limit)
+  end
+
+  def self.favorite_customer(limit = 1, id_params)
+    Customer
+    .select("customers.*, COUNT(transactions.id) AS total_transactions")
+    .joins(invoices: [:transactions])
+    .where("invoices.merchant_id=#{id_params}")
+    .merge(Transaction.successful)
+    .group(:id)
+    .order("total_transactions DESC")
+    .limit(limit)[0]
   end
 end
