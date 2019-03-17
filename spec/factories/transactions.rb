6f9374a658b26c20c762b6c0ca3c3a009b565e5d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-03-16 22:17:55 -0600

    Alters factory to default result: 'failed'.

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     invoice
     sequence :credit_card_number { |n| "92838383883#{n}" }
     credit_card_expiration_date { "" }
-    result { nil }
+    result { 'failed' }
     created_at { (Time.now - 1.day).utc }
     updated_at { (Time.now).utc }
   end
